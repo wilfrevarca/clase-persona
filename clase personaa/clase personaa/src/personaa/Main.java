597808a8132c5b3545adca5e803e076fc2276490
wilfredo <109392939+wilfrevarca@users.noreply.github.com>
2022-08-03 17:16:14 -0500

    Add files via upload

---
+++ clase personaa/clase personaa/src/personaa/Main.java
@@ -0,0 +1,20 @@
+package personaa;
+
+
+    public class Main {
+
+        public static void main(String[] args) {
+            class persona {
+
+            }
+
+
+            String edad;
+            {
+                String nombre;
+            }
+            String telefono;
+
+            System.out.println("edad,nombre,telefono");
+        }
+    }
